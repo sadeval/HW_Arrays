5d200931d763b64d8c63590a78e7537be0b70cef
sadeval <162736837+sadeval@users.noreply.github.com>
2024-06-04 16:12:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace HW_Arrays
+{
+    internal class Zmeyka
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер матрицы ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            int[,] array = new int[size, size];
+            int value = 1;
+
+            for (int i = 0; i < size; i++)
+            {
+                if (i % 2 == 0) 
+                {
+                    for (int j = 0; j < size; j++)
+                    {
+                        array[i, j] = value++;
+                    }
+                }
+                else 
+                {
+                    for (int j = size - 1; j >= 0; j--)
+                    {
+                        array[i, j] = value++;
+                    }
+                }
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Console.Write("{0, 4}", array[i, j]);
+                }
+                Console.WriteLine();
+            }
+        }
+  
+    }
+}
